0d6d433eece390882bb675314537282b2bbf16ea
Carlos Fernandez <fernandezherrera345@gmail.com>
2019-04-07 18:13:03 -0500

    Fixing Customer POST Service validation

--- src/main/java/com/cafh/poc/customer/controller/CustomerRequest.java
+++ src/main/java/com/cafh/poc/customer/controller/CustomerRequest.java
@@ -1,33 +1,35 @@
 package com.cafh.poc.customer.controller;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiParam;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Data
 public class CustomerRequest {
 
-    @ApiModelProperty("Nombre")
+    @JsonProperty("Nombre")
     @ApiParam(value = "Customer Name", type = "string", example ="Carlos")
     @NotNull
     private String name;
 
-    @ApiModelProperty("Apellido")
+    @JsonProperty("Apellido")
     @ApiParam(value = "Customer Last Name", type = "string", example ="Fernandez")
     @NotNull
     private String lastName;
 
-    @ApiModelProperty("Edad")
+    @JsonProperty("Edad")
     @ApiParam(value = "Customer Age", type = "integer", example ="34")
     @NotNull
+    @Min(value = 0)
     private Integer age;
 
-    @ApiModelProperty("Fecha de Nacimiento")
+    @JsonProperty("Fecha de Nacimiento")
     @ApiParam(value = "Customer Date of Birth", type = "long", example ="472626000000")
     @NotNull
+    @Min(value = 0)
     private Long dateOfBirth;
 
 }
